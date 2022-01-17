9c71c2107b55718a098f1a9c80d9880346c982d6
rohanshah <R04@nsha4>
2022-01-17 21:34:57 +0530

    Created Kafka Producer for Spring Boot

---
+++ src/main/java/com/kafka/producer/example/Controller/UserController.java
@@ -0,0 +1,51 @@
+package com.kafka.producer.example.Controller;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.kafka.producer.example.Model.User;
+import com.kafka.producer.example.Service.UserService;
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Properties;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/save")
+    public String saveUser(@RequestBody User user) throws JsonProcessingException {
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            String userAsString = mapper.writeValueAsString(user);
+            ProducerRecord<String, String> record = new ProducerRecord<String, String>("MyTopic", userAsString);
+            producer.send(record, new Callback() {
+                @Override
+                public void onCompletion(RecordMetadata metadata, Exception exception) {
+                    if (exception == null) {
+                        System.out.println("No exceptions");
+                    } else {
+                        System.out.println("Exception while sending");
+                    }
+                }
+            });
+        } finally {
+            producer.close();
+        }
+        return user.getName();
+    }
+}
---
+++ src/main/java/com/kafka/producer/example/Service/UserService.java
@@ -0,0 +1,7 @@
+package com.kafka.producer.example.Service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+}
